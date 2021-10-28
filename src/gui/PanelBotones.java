d6481580d4aff7a255f55c9b59c99a77f57750dd
juancamilobonet2 <j.bonet@uniandes.edu.co>
2021-10-27 20:22:23 -0500

    panel settings y panel botones listo

--- src/gui/PanelBotones.java
+++ src/gui/PanelBotones.java
@@ -1,13 +1,69 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PanelBotones extends JPanel{
 	
 	public PanelBotones(){
-		setSize(new Dimension(100,100));
+		JButton button1 = new JButton("NUEVO");
+		JButton button2 = new JButton("REINICIAR");
+		JButton button3 = new JButton("TOP-10");
+		JButton button4 = new JButton("CAMBIAR JUGADOR");
+		
+		add(button1);
+		add(button2);
+		add(button3);
+		add(button4);
+		
+		button1.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+		        pushNuevo(evt);
+		    }
+		});
+		
+		button2.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+		        pushReiniciar(evt);
+		    }
+		});
+		
+		button3.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+		        pushTop10(evt);
+		    }
+		});
+		
+		button4.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+		        pushCambiar(evt);
+		    }
+		});
+		
+	}
+	
+	private void pushNuevo(ActionEvent evt) {
+		//juego nuevo
+		System.out.println("Button1 clicked");
+	}
+	
+	private void pushReiniciar(ActionEvent evt) {
+		//Reiniciar juego
+		System.out.println("Button2 clicked");
+	}
+	
+	private void pushTop10(ActionEvent evt) {
+		//Menu top 10
+		System.out.println("Button2 clicked");
+	}
+	
+	private void pushCambiar(ActionEvent evt) {
+		//Cambiar jugador
+		System.out.println("Button2 clicked");
 	}
 
 }
--- src/gui/PanelSettings.java
+++ src/gui/PanelSettings.java
@@ -1,12 +1,90 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class PanelSettings extends JPanel {
+	
+	private int tamanio;
+	
+	private JRadioButton option1;
+    private JRadioButton option2;
+    private JRadioButton option3;
+	
+	
 
 	public PanelSettings() {
-		setSize(new Dimension(100,100));
+		//Para escoger tamaño
+		JLabel label = new JLabel("Tamaño: ");
+		add(label);
+		
+		String[] tamanios = new String[] {"4x4","5x5","6x6"};
+		JComboBox<String> tamaniosList = new JComboBox<>(tamanios);
+		add(tamaniosList);
+		
+		//Para escoger dificultad
+		label = new JLabel("Dificultad: ");
+		add(label);
+		
+		option1 = new JRadioButton("Facil");
+        option2 = new JRadioButton("Medio");
+        option3 = new JRadioButton("Dificil");
+        
+        option1.setBackground(Color.blue);
+        option2.setBackground(Color.blue);
+        option3.setBackground(Color.blue);
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(option1);
+        group.add(option2);
+        group.add(option3);
+        
+        add(option1);
+        add(option2);
+        add(option3);
+        
+        
+        tamaniosList.addActionListener(new ActionListener() {
+        	 
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                JComboBox<String> combo = (JComboBox<String>) event.getSource();
+                String selectedBook = (String) combo.getSelectedItem();
+         
+                if (selectedBook.equals("4x4")) {
+                    tamanio = 4;
+                } else if (selectedBook.equals("5x5")) {
+                    tamanio = 5;
+                } else if (selectedBook.equals("6x6")) {
+                    tamanio = 6;
+                }
+            }
+        });
+	}
+	
+	public int getDificultad() {
+		int dif = 0;
+		if(option1.isSelected()) {
+			dif = 1;
+		}
+		if(option2.isSelected()) {
+			dif = 2;
+		}
+		if(option3.isSelected()) {
+			dif = 3;
+		}
+		return dif;
+	}
+	
+	public int getTamanio() {
+		return tamanio;
 	}
 }
